e40f7b477425d23be2b47ae5b48927e1d58a76d2
helmiruza <helmiruza@gmail.com>
2015-04-06 11:52:45 +0000

    added pacman ajax loader gif

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,26 +1,19 @@
-require 'twitter'
-
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
 post '/create' do
-	@user = TwitterUser.find_by_username(params[:user][:username])
+	@user = TwitterUser.find_or_create_by(username: params[:user][:username])
 
-	if @user.nil?
-		@user = TwitterUser.create(username: params[:user][:username])
+	if (@user.tweets.empty? || @user.stale?)
+		@user.fetch_tweets!(@user.username)
+		
 	end
-
-	redirect to "/#{@user.username}"
+	return @user.username
 end
 
 get '/:username' do |username|
 	@user = TwitterUser.find_by_username(username)
-	
-	if (@user.tweets.empty? || @user.stale?)
-		@user.fetch_tweets!(username)
-	end
 		   
 	@tweets = @user.tweets
 	erb :display
